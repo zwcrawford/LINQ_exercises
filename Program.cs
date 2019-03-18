524850bfa0a02291bb399cdbd44fd3d71e4f58d7
Zac Crawford <zwcrawford@gmail.com>
2019-03-18 13:16:47 -0500

    exercise one

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_exercises
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+			// 1. Find the words in the collection that start with the letter 'L'
+			List<string> fruits = new List<string>()
+			{
+				"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"
+			};
+
+			IEnumerable<string> LFruits = from fruit in fruits
+				where fruit.StartsWith('L')
+				orderby fruit ascending
+				select fruit;
+
+			foreach(string fruit in LFruits)
+			{
+				Console.WriteLine($"{fruit}");
+			}
+		}
+	}
+}
