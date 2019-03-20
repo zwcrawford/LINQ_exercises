0d39b3e158a7143a01adcfa642484d316947b13a
Zac Crawford <zwcrawford@gmail.com>
2019-03-20 00:25:45 -0500

    done for now

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_exercises
+{
+    class Customer
+	{
+		static void Main()
+		{
+      public string Name;
+      public double Balance;
+      public string Bank;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace linq_exercises
 {
     class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
 			// 1. Find the words in the collection that start with the letter 'L'
 			List<string> fruits = new List<string>()
@@ -126,6 +126,35 @@ namespace linq_exercises
 				Console.WriteLine(number);
 			}
 			Console.WriteLine(); // Blank line.
+
+			// *** COMING BACK FOR THIS ***
+			/*
+			9. Given the same customer set, display how many millionaires per bank.
+				 Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+				 Example Output:
+				 WF 2
+				 BOA 1
+				 FTB 1
+				 CITI 1
+			*/
+
+			// List of customers
+			// List<Customer> customers = new List<Customer>()
+			// {
+			// 	new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+			// 	new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+			// 	new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+			// 	new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+			// 	new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+			// 	new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+			// 	new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+			// 	new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+			// 	new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+			// 	new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+			// };
+
+			// IEnumerable<Entry>
 		}
 	}
 }
